2d1abbfad744fd328c8afee5cc19107a842c7f09
abilimisi <asbadarsh21@gmail.com>
2024-12-11 13:04:35 +0530

    updated

--- array/check_sorted_array.py
+++ array/check_sorted_array.py
@@ -1,14 +1,13 @@
 def sorted_array(sorted_arr):
-    for i in range(1,len(sorted_arr)):
-        if sorted_arr[i-1] > sorted_arr[i]:
+    for i in range(1, len(sorted_arr)):
+        if sorted_arr[i - 1] > sorted_arr[i]:
             return False
     else:
         return True
 
+
 arr = [1, 2, 2, 4, 7, 9]
-if(sorted_array(arr)):
+if sorted_array(arr):
     print("True")
 else:
     print("false")
-
-
