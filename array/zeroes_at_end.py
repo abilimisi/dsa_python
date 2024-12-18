89b650d740a346519b21c00152b89c6c75bad942
abilimisi <asbadarsh21@gmail.com>
2024-12-18 23:10:04 +0530

    updated

---
+++ array/zeroes_at_end.py
@@ -0,0 +1,36 @@
+# def zeroes_at_end(arr):
+#     n = len(arr)
+#     for i in range(len(arr)):
+#         if arr[i] == 0:
+#             element = arr.pop(i)
+#             arr.insert(n, element)
+#     return arr
+#
+# arr = [1,0,6,4,0,5]
+#
+# result = zeroes_at_end(arr)
+# print(result)
+
+def zeroes_at_end(arr):
+    temp = [0] * len(arr)  # temp is initialized with all zeroes [0,0,0,0,0,0]
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            temp[j] = arr[i]
+            j += 1
+    return temp  # have to return temp not arr
+
+
+arr = [1, 0, 6, 4, 0, 5]
+
+result = zeroes_at_end(arr)
+print(result)
+
+# working scenario
+
+# If arr = [1, 0, 6, 4, 0, 5], temp starts as [0, 0, 0, 0, 0, 0].
+# During the loop:
+# temp[0] = 1 → temp = [1, 0, 0, 0, 0, 0]
+# temp[1] = 6 → temp = [1, 6, 0, 0, 0, 0]
+# temp[2] = 4 → temp = [1, 6, 4, 0, 0, 0]
+# temp[3] = 5 → temp = [1, 6, 4, 5, 0, 0]
