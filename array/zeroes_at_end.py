3a8fc50867bd86316d7dc211a5142a94f4c6f434
abilimisi <asbadarsh21@gmail.com>
2024-12-18 23:36:11 +0530

    updated

--- array/zeroes_at_end.py
+++ array/zeroes_at_end.py
@@ -1,17 +1,19 @@
-# def zeroes_at_end(arr):
-#     n = len(arr)
-#     for i in range(len(arr)):
-#         if arr[i] == 0:
-#             element = arr.pop(i)
-#             arr.insert(n, element)
-#     return arr
-#
-# arr = [1,0,6,4,0,5]
-#
-# result = zeroes_at_end(arr)
-# print(result)
+"""def zeroes_at_end(arr):
+    n = len(arr)
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            element = arr.pop(i)
+            arr.insert(n, element)
+    return arr
 
-def zeroes_at_end(arr):
+arr = [1,0,6,4,0,5]
+
+result = zeroes_at_end(arr)
+print(result)
+"""
+#-------------------------------------------------------------------
+
+"""def zeroes_at_end(arr):
     temp = [0] * len(arr)  # temp is initialized with all zeroes [0,0,0,0,0,0]
     j = 0
     for i in range(len(arr)):
@@ -25,7 +27,7 @@ arr = [1, 0, 6, 4, 0, 5]
 
 result = zeroes_at_end(arr)
 print(result)
-
+"""
 # working scenario
 
 # If arr = [1, 0, 6, 4, 0, 5], temp starts as [0, 0, 0, 0, 0, 0].
@@ -34,3 +36,23 @@ print(result)
 # temp[1] = 6 → temp = [1, 6, 0, 0, 0, 0]
 # temp[2] = 4 → temp = [1, 6, 4, 0, 0, 0]
 # temp[3] = 5 → temp = [1, 6, 4, 5, 0, 0]
+
+
+#----------------------------------------------------------
+
+def z_at_end(arr):
+    count = 0
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count+=1
+
+    while count < len(arr):
+        arr[count] = 0
+        count+=1
+    return arr
+arr = [1, 0, 6, 4, 0, 5]
+
+result = z_at_end(arr)
+print(result)
