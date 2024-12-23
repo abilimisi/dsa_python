711e36236f5c48ebba8bc1f563e297d379e770f7
abilimisi <asbadarsh21@gmail.com>
2024-12-23 16:01:32 +0530

    added one file

---
+++ selection_sort.py
@@ -0,0 +1,19 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+
+        min_val = i
+
+        for j in range(i + 1, n):
+
+            if arr[j] < arr[min_val]:
+                min_val = j
+
+        arr[i], arr[min_val] = arr[min_val], arr[i]
+
+
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+for res in arr:
+    print(res, end=" ")
+print()
