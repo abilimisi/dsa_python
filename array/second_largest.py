0b83973870834fd636dde7880a4df09d7124b6e1
abilimisi <asbadarsh21@gmail.com>
2024-12-27 09:58:44 +0530

    upd

--- array/second_largest.py
+++ array/second_largest.py
@@ -1,28 +1,36 @@
-# def second_largest(arr):
-#     arr.sort()
-#
-#     z = arr[-2]
-#     if z != arr[0]:
-#         return z
-#     else:
-#         return -1
-#
-# arr = [12,35,1,10,34]
-#
-# result = second_largest(arr)
-# print(result)
-
+# O(n*logn) Time and O(1) Space
 def second_largest(arr):
     arr.sort()
 
     n = len(arr)
 
-    for i in range(n-2,-1,-1):
+    for i in range(n - 2, -1, -1):
         if arr[i] != arr[0]:
             return arr[i]
     return -1
 
-arr = [12,37,14,48,23]
+
+arr = [12, 37, 14, 48, 23]
 
 result = second_largest(arr)
 print(result)
+
+
+# --------------------------------------------------------------------
+
+# O(n) Time and O(1) Space
+def sec_lar(arr):
+    largest = -1
+    s_largest = -1
+
+    for i in arr:
+        if i > largest:
+            largest = i
+    for j in arr:
+        if j > s_largest and j != largest:
+            s_largest = j
+        return s_largest
+
+
+arr = [12, 37, 14, 48, 23]
+print(second_largest(arr))
