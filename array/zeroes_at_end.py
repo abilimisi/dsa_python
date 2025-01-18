b8bf876695fda9c233a6c6df404c8d3aba9dec9a
abilimisi <asbadarsh21@gmail.com>
2025-01-18 23:14:55 +0530

    updated

--- array/zeroes_at_end.py
+++ array/zeroes_at_end.py
@@ -11,7 +11,7 @@ arr = [1,0,6,4,0,5]
 result = zeroes_at_end(arr)
 print(result)
 """
-#-------------------------------------------------------------------
+# -------------------------------------------------------------------
 
 """def zeroes_at_end(arr):
     temp = [0] * len(arr)  # temp is initialized with all zeroes [0,0,0,0,0,0]
@@ -28,6 +28,8 @@ arr = [1, 0, 6, 4, 0, 5]
 result = zeroes_at_end(arr)
 print(result)
 """
+
+
 # working scenario
 
 # If arr = [1, 0, 6, 4, 0, 5], temp starts as [0, 0, 0, 0, 0, 0].
@@ -38,7 +40,7 @@ print(result)
 # temp[3] = 5 → temp = [1, 6, 4, 5, 0, 0]
 
 
-#----------------------------------------------------------
+# ----------------------------------------------------------
 
 def z_at_end(arr):
     count = 0
@@ -46,12 +48,14 @@ def z_at_end(arr):
     for i in range(len(arr)):
         if arr[i] != 0:
             arr[count] = arr[i]
-            count+=1
+            count += 1
 
     while count < len(arr):
         arr[count] = 0
-        count+=1
+        count += 1
     return arr
+
+
 arr = [1, 0, 6, 4, 0, 5]
 
 result = z_at_end(arr)
