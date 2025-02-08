2ab42e653d6819f008da16a26fcc603fa825a49d
abilimisi <asbadarsh21@gmail.com>
2025-02-08 18:44:52 +0530

    updated

--- array/second_largest.py
+++ array/second_largest.py
@@ -14,3 +14,22 @@ def sec_lar(arr):
 
 arr = [12, 37, 14, 48, 23]
 print(sec_lar(arr))
+
+
+
+def sec_lar_element(arr_elements):
+    largest = float('-inf')
+    s_largest = float('-inf')
+
+    for i in arr_elements:
+        if i > largest:
+            s_largest = largest
+            largest = i
+
+        elif(i>s_largest and i != largest):
+            s_largest = i
+    return s_largest
+
+
+arr_elements = [12, 37, 14, 48, 23]
+print(sec_lar_element(arr_elements))
