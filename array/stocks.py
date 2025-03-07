8275f23e8cf2d38b69dd01af69bde4f92b615ebb
Adarsh Bilimisi <asbadarsh21@gmail.com>
2025-03-07 19:46:54 +0530

    updated

--- array/stocks.py
+++ array/stocks.py
@@ -3,7 +3,7 @@ def maximumProfit(prices):
     for i in range(1, len(prices)):
         if prices[i] > prices[i - 1]:
             profit += prices[i] - prices[i - 1]
-            print(profit)
+
     return profit
 
 
