0b91c11c5873427d116ee209657b59d6b941ec2c
Adarsh Bilimisi <asbadarsh21@gmail.com>
2025-04-11 22:51:13 +0530

    updated

--- array/fraaz/target_finding.py
+++ array/fraaz/target_finding.py
@@ -2,13 +2,16 @@ def find_terget(nums,target):
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if(nums[i]+nums[j] == target):
-                print("Found")
-                return  #use return not break
-    print("Not Found")
+                return True #use return not break
+    else:
+        return False
 
-nums = [1,2,3,4,5,6]
+nums = [1,2,4,5,6]
 target = 11
 
-find_terget(nums,target)
+res = find_terget(nums,target)
 
-
+if(res):
+    print("Found")
+else:
+    print("Not Found")
